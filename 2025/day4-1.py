733b34a693629c4dcfc71c7c6778e20f9aa0bb92
Paul Bourke <pauldbourke@protonmail.com>
2025-12-07 16:23:52 +0000

    2025: tidy up day 4 part 1

--- 2025/day4-1.py
+++ 2025/day4-1.py
@@ -1,44 +1,19 @@
 
-grid = []
-
 with open('input', 'r') as f:
-    for line in f:
-        entry = []
-        grid.append([x for x in line.strip()])
+    grid = [list(line.strip()) for line in f]
 
-
-N = len(grid[0])
-M = len(grid)
 count = 0
+# northwest, north, northeast, west, east, southwest, south, southeast
+positions = ((-1, -1), (-1, 0), (-1, 1), (0, -1), (0, 1), (1, -1), (1, 0), (1, 1))
 
-for row in range(M):
-    for col in range(N):
-        adjacent_count = 0
+for row in range(len(grid)):
+    for col in range(len(grid[0])):
         if grid[row][col] == '@':
-            # north
-            if row > 0 and grid[row-1][col] == '@':
-                adjacent_count += 1
-            # north-east
-            if row > 0 and col < N-1 and grid[row-1][col+1] == '@':
-                adjacent_count += 1
-            # east
-            if col < N-1 and grid[row][col+1] == '@':
-                adjacent_count += 1
-            # south-east
-            if col < N-1 and row < M-1 and grid[row+1][col+1] == '@':
-                adjacent_count += 1
-            # south
-            if row < M-1 and grid[row+1][col] == '@':
-                adjacent_count += 1
-            # south-west
-            if row < M-1 and col > 0 and grid[row+1][col-1] == '@':
-                adjacent_count += 1
-            # west
-            if col > 0 and grid[row][col-1] == '@':
-                adjacent_count += 1
-            # north-west
-            if row > 0 and col > 0 and grid[row-1][col-1] == '@':
-                adjacent_count += 1
+            adjacent_count = 0
+            for dr, dc in positions:
+                r, c = row + dr, col + dc
+                if 0 <= r < len(grid) and 0 <= c < len(grid[0]) and grid[r][c] == '@':
+                    adjacent_count += 1
 
             if adjacent_count < 4:
                 count += 1
